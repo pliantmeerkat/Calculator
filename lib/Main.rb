22e13ed47b1d79f7937b9d2605c9ca37be30de73
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-13 17:33:10 +0100

    end of day 1

--- lib/Main.rb
+++ lib/Main.rb
@@ -1,21 +1,30 @@
 # this is the file for the main program, which handles the gui running and user input
-
 require 'fox16'
 include Fox 
 
 class GUI < FXMainWindow # child of FXMainwindow
 
-    def initialize(app) # runs the application on opbject creation
-        main = FXMainWindow.new(app, "Calculator", :width => 300, :height => 400)
+    attr_reader :calc
+    def initialize()
+        @calc = Calc.new    
     end
 
     def create
         super
         show(PLACEMENT_SCREEN)
     end
+
+    def add 
+            
+    end
+
 end
 
+
+begin
 app = FXApp.new
-GUI.new(app)
+main = FXMainWindow.new(app, "Calculator", :width => 300, :height => 400)    
+GUI.new
 app.create
 app.run
+end
--- spec/Main_spec.rb
+++ spec/Main_spec.rb
@@ -1,16 +1,22 @@
 require './lib/Main'
+require 'fox16'
+include Fox
 
 describe GUI do
 
     it 'GUI exists' do
-        expect(subject).to be_an(FXApp) # should be a fx application
+        expect(subject).to be_an(FXMainWindow) # should be a fx application
     end
     it 'can create application' do
         expect(subject).to respond_to(:create) # should have a method called create
     end
     
     describe 'GUI Features' do
-        # expect(subject)
+        describe 'buttons' do
+            it 'has an add button that works' do
+               expect(subject).to respond_to(:add) 
+            end
+        end
     end
 
 end
